a4fd6c5cd129e95856f9e36f9360c676b42d8fb9
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2026-01-16 18:59:09 +0530

    feat: implement initial server application with Guice dependency injection and Jersey REST services.

--- server/src/main/java/code/apiversion/Main.java
+++ server/src/main/java/code/apiversion/Main.java
@@ -1,7 +1,15 @@
 package code.apiversion;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import code.apiversion.controller.ExampleController;
+import code.apiversion.injection.AppModule;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Injector injector = Guice.createInjector(new AppModule());
+        ExampleController exampleController = injector.getInstance(ExampleController.class);
+        System.out.println(exampleController.getExample());
     }
 }
